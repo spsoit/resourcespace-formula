ffd0b4f844e75e42789bb8b4fef1c760798a46ba
Chad Heuschober <chad.heuschober@cuny.edu>
2014-05-10 07:57:36 -0400

    Pass two on a php block template

--- resourcespace/files/config.php
+++ resourcespace/files/config.php
@@ -1,3 +1,22 @@
+{%- macro quote_val(value) %}
+{% if value is number -%}
+  {{ value }}
+{%- else -%}
+  '{{ value|replace("'", "\'"}}'
+{%- endif %}
+{% endmacro -%}
+
+{%- macro php_block(key, value, operator='=', delim=';', keypre = '$', ind=0) %}
+    {%- if value is iterable: -%}
+        {{ keypre|indent(ind) }}{{ key }} {{ operator }} array(
+        {%- for k, v in value.items() %}
+            {{ php_block(k, v, '=>', ',', '', (ind + 4) }}
+        {% endfor -%}
+        {{ ')'|indent(ind) }}{{delim}}
+    {%- else -%}
+        {{ keypre|indent(ind) }}{{ quote_val(key) }} {{ operator }} {{ value }}{{ delim }}
+    {%- endif -%}
+{% endmacro -%}
 <?php
 /**
  * This file contains the default configuration settings.
@@ -12,9 +31,5 @@
  */
 
 {% for key,value in config.items() %}
-    {%- if value is string: -%}
-        ${{key}} = '{{value|replace("'","\'")}}';
-    {%- else -%}
-        ${{key}} = {{value}};
-    {%- endif -%}
+{{ php_block(key, value) }}
 {% endfor %}
